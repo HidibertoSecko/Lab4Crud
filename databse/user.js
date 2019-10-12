3525e80a26033101953fb14df155ad20a1582c75
HidibertoSecko <hidibertosecko@gmail.com>
2019-10-12 04:59:54 -0700

    laboratorio 4

---
+++ databse/user.js
@@ -0,0 +1,11 @@
+var mongoose = require("./connect");
+var USERSCHEMA = {
+      name :        String,
+      email :       String,
+      password :    String,
+      registerdate: Date,
+      sex :         String,
+      address :     String
+};
+const USERS= mongoose.model("user", USERSCHEMA);
+module.exports = USERS;
